6641d3458b0a4f2a6adaa6b527fae6321c238291
Laura <lauracolof@gmail.com>
2023-05-19 14:07:58 -0300

    add ./components/Detail

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -17,5 +17,23 @@ export default function Details() {
     });
   }, []);
 
-  return <div className=''></div>;
+  return (
+    <div className='detail-container'>
+      {product ? (
+        <div className='info'>
+          <div className='detail-image'>
+            <img src={product.image} alt={product.title} />
+          </div>
+
+          <div className='detail-info'>
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <p>Condition: {product.condition}</p>
+            <p>Price: {product.price}</p>
+            <p>Stock: {product.stock}</p>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
 }
